bd09dfdaada8aa8bf7c410787275f3b7e4b25222
Clement Malarme <clement.malarme@gmail.com>
2021-09-07 10:32:24 +0200

    use state

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Like from "./components/Like";
+import FrameworksList from "./components/FrameworksList";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
           <Like/>
+          <FrameworksList/>
       </header>
     </div>
   );
